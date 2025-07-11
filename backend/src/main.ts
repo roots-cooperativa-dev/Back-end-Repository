323af8cadc4b8daec206e8210aa8133f233c818a
andresdelossantos99 <andresdelossantos99@gmail.com>
2025-07-11 18:39:55 -0300

    implementacion de swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDoc = new DocumentBuilder()
+    .setTitle('Proyecto Final Roots Cooperativa')
+    .setVersion('1.0.0')
+    .setDescription('Sitio Web Cooperativa de Trabajo Roots ltda')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerDoc);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
