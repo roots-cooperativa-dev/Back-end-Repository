e5b4f2ba952cef95a2e3d1939743c825e50c994d
Lucio Moran <ezeespa78@gmail.com>
2025-07-18 15:52:43 -0300

    Revert "Feature/lucio moran back end"

--- backend/src/config/google-0auth.config.ts
+++ backend/src/config/google-0auth.config.ts
@@ -3,9 +3,8 @@ import { config as dotenvConfig } from 'dotenv';
 
 dotenvConfig({ path: '.env.development' });
 
-export default registerAs('GoogleOAuth', () => ({
+export default registerAs('Google0Auth', () => ({
   clientId: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackUrl: process.env.GOOGLE_CALLBACK_URL,
-  frontendUrl: process.env.FRONTEND_CLOUD_URL,
 }));
--- backend/src/modules/auths/auths.module.ts
+++ backend/src/modules/auths/auths.module.ts
@@ -1,30 +1,28 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { UsersModule } from '../users/users.module';
 import { AuthsService } from './auths.service';
 import { AuthsController } from './auths.controller';
 import { AuthGuard } from '../../guards/auth.guards';
 import { GoogleStrategy } from './strategies/google.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
 import google0authConfig from 'src/config/google-0auth.config';
 
 @Module({
   imports: [
     forwardRef(() => UsersModule),
     PassportModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('SUPABASE_JWT_SECRET'),
-      }),
+    JwtModule.register({
+      secret: process.env.SUPABASE_JWT_SECRET,
     }),
-    ConfigModule.forFeature(google0authConfig),
+    ConfigModule.forRoot({
+      load: [google0authConfig],
+      isGlobal: true,
+    }),
   ],
   providers: [AuthsService, AuthGuard, GoogleStrategy],
   controllers: [AuthsController],
-  exports: [JwtModule, AuthGuard, AuthsService],
+  exports: [JwtModule, AuthGuard],
 })
 export class AuthsModule {}
