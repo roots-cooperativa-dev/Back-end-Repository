aedf2ad0a4eaa18fa0875257414775a3c4d65765
luciarobert6 <luciarobert6@gmail.com>
2025-07-25 13:48:21 -0300

    funcionamiento al 100% de products

--- backend/src/modules/products/DTO/CreateProduct.dto.ts
+++ backend/src/modules/products/DTO/CreateProduct.dto.ts
@@ -9,6 +9,7 @@ import {
 import { Type } from 'class-transformer';
 import { CreateProductSizeDTO } from './CreateProductSize.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { File } from 'src/modules/file-upload/entity/file-upload.entity';
 
 export class CreateProductDTO {
   @ApiProperty({ example: 'Remera Edición Limitada' })
@@ -31,30 +32,40 @@ export class CreateProductDTO {
   @Type(() => CreateProductSizeDTO)
   sizes: CreateProductSizeDTO[];
 
-  @ApiProperty({ type: [String], description: 'Array of file IDs' })
+  @ApiProperty({
+    example: '3496gi237082hd09dhfos02r',
+    description: 'Array of file IDs',
+  })
   @IsArray()
   @IsUUID('all', { each: true })
   file_Ids: string[];
 }
 
 export class UpdateProductDTO {
+  @ApiProperty({ example: 'Remera Edición Limitada' })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiProperty({ example: 'Remera de algodón orgánico con diseño exclusivo' })
   @IsOptional()
   @IsString()
   details?: string;
 
+  @ApiProperty({ example: 'fglkafdgb123dfsg864' })
   @IsOptional()
   @IsUUID()
   category_Id?: string;
 
+  @ApiProperty({
+    example: '3496gi237082hd09dhfos02r',
+    description: 'Array of file IDs',
+  })
   @IsOptional()
-  @IsArray()
-  @IsUUID()
-  file_Ids: string[];
+  @IsUUID('4', { each: true })
+  file_Ids?: File[];
 
+  @ApiProperty({ type: [CreateProductSizeDTO] })
   @IsOptional()
   @ValidateNested({ each: true })
   @Type(() => CreateProductSizeDTO)
